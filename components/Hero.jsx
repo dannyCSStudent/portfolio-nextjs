e0e1d0a8410b2268ea4d32d9de182f9164286b49
dannyCSStudent <danv8605@gmail.com>
2023-08-23 10:00:59 -0600

    Edit Hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
         <div>
         <h1 className="font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2">Hi, I'm <span className='text-[#915eff]'>Dan</span></h1>
           <p className="text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2">
-            I develope social media, Ecommerce, <br className='sm:block hidden' />  and web applications
+            I develope social media, Ecommerce, <br className='sm:block hidden' />  and leading page applications
 
           </p>
         </div>
